d057a00dc7d57d752ca476bdf8e8fba33ef50393
0unMinuP <samouo12318@163.com>
2021-06-05 21:11:59 +0800

    图片素材

---
+++ LoveFreshBeen/LoveFreshBeen/Classes/AD/ADViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ADViewController.swift
+//  LoveFreshBeen
+//
+//  Created by Gao on 17/2/7.
+//  Copyright © 2017年 Odm. All rights reserved.
+//
+
+import UIKit
+
+class ADViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
