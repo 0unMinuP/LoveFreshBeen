83d49d78bb8826aaea03775fbe8ecc8da40a4dd1
0unMinuP <samouo12318@163.com>
2021-06-16 15:06:56 +0800

    使用YYModel，Json转对象

--- LoveFreshBeen/LoveFreshBeen/Classes/AD/Model/MainAD.swift
+++ LoveFreshBeen/LoveFreshBeen/Classes/AD/Model/MainAD.swift
@@ -7,27 +7,23 @@
 //
 
 import UIKit
+import YYModel
 
-class MainAD: NSObject {
-    var code: Int = -1
-    var msg: String?
-    var data: AD?
-    
-    class func loadADData(_ completion:(_ data: MainAD?, _ error: NSError?) -> Void) {
-        let path = Bundle.main.path(forResource: "AD", ofType: nil)
-        let data = try? Data(contentsOf: URL(fileURLWithPath: path!))
-        if data != nil {
-//            let dict: NSDictionary = (try! JSONSerialization.jsonObject(with: data!, options: .allowFragments)) as! NSDictionary
-//            let modelTool = DictModelManager.sharedManager
-//            let data = modelTool.objectWithDictionary(dict, cls: MainAD.self) as? MainAD
-//            completion(data, nil)
-        }
-    }
-    
-    static func customClassMapping() -> [String : String]? {
-        return ["data" : "\(AD.self)"]
-    }
-}
+// -----------------------------
+//{
+//    "code": 0,
+//    "msg": "success",
+//    "reqid": "933552221c3e63423e081b932739dba7",
+//    "data": {
+//        "title": "",
+//        "img_name": "http:\/\/img01.bqstatic.com\/upload\/activity\/2016011111271995.jpg",
+//        "img_big_name": "http:\/\/img01.bqstatic.com\/upload\/activity\/2016011111271981.jpg",
+//        "img_url": "",
+//        "starttime": "1450409894",
+//        "endtime": "1454255999"
+//    }
+//}
+// -----------------------------
 
 class AD: NSObject {
     var title: String?
@@ -35,3 +31,39 @@ class AD: NSObject {
     var starttime: String?
     var endtime: String?
 }
+
+
+
+class MainAD: NSObject {
+    var code: Int = -1
+    var msg: String?
+    var data: AD?
+    
+    // 返回容器类中的所需要存放的数据类型 (以 Class 或 Class Name 的形式)。
+    class func modelContainerPropertyGenericClass() -> [AnyHashable: Any] {
+        return ["data": AD.self]
+    }
+    
+    
+    class func loadADData(_ completion:(_ data: MainAD?, _ error: NSError?) -> Void) {
+        
+        guard let path = Bundle.main.path(forResource: "AD", ofType: nil) else{
+            return
+        }
+
+        let url = URL(fileURLWithPath: path)
+        
+        guard let jsonString = try? String(contentsOf: url) else{
+            return
+        }
+
+        let mainAD = MainAD.yy_model(withJSON: jsonString)
+        
+        if mainAD != nil {
+            completion(mainAD, nil)
+        }
+
+    }
+}
+
+
